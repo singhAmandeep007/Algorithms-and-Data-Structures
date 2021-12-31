c613a42796d4188881c8c8ff980e94c2ed64753c
Grimm0803 <55043082+amandeepmicro@users.noreply.github.com>
2021-12-31 10:54:56 +0530

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -1,27 +1,28 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+/* --- Directions
+Given a string, return true if the string is a palindrome
+or false if it is not.  Palindromes are strings that
+form the same word if it is reversed. *Do* include spaces
+and punctuation in determining if the string is a palindrome.
+--- Examples:
+  palindrome("abba") === true
+  palindrome("abcdefg") === false
+ */
 
-// 1st Solution 
-// function palindrome(str) {
-//     let reversed = str.split('').reverse().join('');
-//     return reversed === str;
-// }
+// 1st Solution
+function palindrome1(str) {
+  let reversed = str.split('').reverse().join('');
+  return reversed === str;
+}
 /* 
     we first convert the string into array and then using built-in reverse method we reverse it and then join the array to form a single string.
     Then we compare the string passed as argument and the reversed string , which returns a boolean value.
 */
 
-// 2nd Solution 
-function palindrome(str) {
-    return str.split('').every((item, index) => {
-        return item === str[str.length - index - 1];
-    })
+// 2nd Solution
+function palindrome2(str) {
+  return str.split('').every((item, index) => {
+    return item === str[str.length - index - 1];
+  });
 }
 /* 
    The every() method checks if all elements in an array pass a test (provided as a function).
@@ -33,4 +34,40 @@ function palindrome(str) {
     Here we first convert the string into array then using every we compare each character with the mirror character .
 */
 
-module.exports = palindrome;
+//3rd Solution
+function palindrome3(str) {
+  let reverseStr = '';
+  let p1 = str.length;
+  for (let i = 1; i <= str.length; i++) {
+    reverseStr += str[p1 - i];
+  }
+  return reverseStr === str;
+}
+/* 
+	here we use simple loop and pointer p1 to keep track of index.
+*/
+
+// 4th Solution
+function palindrome4(str) {
+  let reversedStr = str.split('').reduceRight((acc, curr) => {
+    return acc + curr;
+  });
+  return str === reversedStr;
+}
+/* 
+	here we run the reduce function starting with values from right to left
+*/
+
+// 5th Solution
+function palindrome5(str) {
+  return str.split('').reduce((acc, curr) => curr + acc, '') === str;
+}
+/* Here we do curr + acc  */
+
+module.exports = [
+  palindrome1,
+  palindrome2,
+  palindrome3,
+  palindrome4,
+  palindrome5,
+];
