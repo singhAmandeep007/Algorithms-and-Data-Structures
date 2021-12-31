42d2da68722a13f2c048127d136c59cc6488f38a
Grimm0803 <55043082+amandeepmicro@users.noreply.github.com>
2021-12-31 10:55:12 +0530

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,56 +1,55 @@
-// --- Directions
-// Given a string, return the character that is most
-// commonly used in the string.
-// --- Examples
-// maxChar("abcccccccd") === "c"
-// maxChar("apple 1231111") === "1"
+/* --- Directions
+Given a string, return the character that is most
+commonly used in the string.
+--- Examples
+maxChar("abcccccccd") === "c"
+maxChar("apple 1231111") === "1" */
 
-// 1st solution
-// function maxChar(str) {
-//     const charMap = {};
-//     let max = 0;
-//     let maxChar = '';
+// 1st Solution
+function maxChar1(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
 
-//     for (let c of str) {
-//         if (charMap[c]) {
-//             charMap[c]++;
-//         } else {
-//             charMap[c] = 1;
-//         }
-//     }
-
-//     for (let i in charMap) {
-//         if (max < charMap[i]) {
-//             max = charMap[i];
-//             maxChar = i;
-//         } else {
-//             continue;
-//         }
-//     }
-//     return maxChar;
-// }
-
-/* 
-    we first create an empty object then we map each character in the string to the object. Thus creating a key : value properties of the object. Then we use for loop to find max value and its corroponding key.
-*/
-
-// 2nd solution
-function maxChar(str) {
-    const chars = {};
-    for (let s of str) {
-        if (chars[s]) {
-            chars[s]++;
-        } else {
-            chars[s] = 1;
-        }
+  for (let c of str) {
+    if (charMap[c]) {
+      charMap[c]++;
+    } else {
+      charMap[c] = 1;
     }
-    let maxValue = Math.max(...Object.values(chars));
-    let maxChar = Object.keys(chars).find((el) => {
-        return chars[el] === maxValue;
-    })
-    return maxChar;
+  }
+
+  for (let i in charMap) {
+    if (max < charMap[i]) {
+      max = charMap[i];
+      maxChar = i;
+    } else {
+      continue;
+    }
+  }
+  return maxChar;
 }
 
+/* 
+    we first create an empty object then we map each character in the string to the object. Thus creating a key : value properties of the object. Then we use for loop to find max value and its corresponding key.
+*/
+
+// 2nd Solution
+function maxChar2(str) {
+  const chars = {};
+  for (let s of str) {
+    if (chars[s]) {
+      chars[s]++;
+    } else {
+      chars[s] = 1;
+    }
+  }
+  let maxValue = Math.max(...Object.values(chars));
+  let maxChar = Object.keys(chars).find((el) => {
+    return chars[el] === maxValue;
+  });
+  return maxChar;
+}
 
 /* 
     For plain objects, the following methods are available:
@@ -67,4 +66,22 @@ function maxChar(str) {
     Then using find method we return key whose value matches maxValue;
 */
 
-module.exports = maxChar;
+// 3rd Solution
+function maxChar3(str) {
+  let maxChar = '';
+  [...str].reduce((acc, curr) => {
+    let obj = Object.assign(acc, { [curr]: acc[curr] ? (acc[curr] += 1) : 1 });
+    maxChar = Object.keys(obj).find(
+      (el) => obj[el] === Math.max(...Object.values(obj))
+    );
+    return obj;
+  }, {});
+  return maxChar;
+}
+
+/* 
+	Here we leverage the reduce fn to accumulate count of each character and use it to get max in next iteration
+
+*/
+
+module.exports = [maxChar1, maxChar2, maxChar3];
