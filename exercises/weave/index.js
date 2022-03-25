08fbd67ffe422bb9db3290048b43aa16fce6c8e8
Grimm0803 <55043082+amandeepmicro@users.noreply.github.com>
2022-03-25 22:41:49 +0530

    weaving queue

---
+++ exercises/weave/index.js
@@ -0,0 +1,42 @@
+// --- Directions
+// 1) Complete the task in weave/queue.js
+// 2) Implement the 'weave' function.  Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// The third queue should contain the *alterating* content
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
+// *Do not* access the array inside of any queue, only
+// use the 'add', 'remove', and 'peek' functions.
+// --- Example
+//    const queueOne = new Queue();
+//    queueOne.add(1);
+//    queueOne.add(2);
+//    const queueTwo = new Queue();
+//    queueTwo.add('Hi');
+//    queueTwo.add('There');
+//    const q = weave(queueOne, queueTwo);
+//    q.remove() // 1
+//    q.remove() // 'Hi'
+//    q.remove() // 2
+//    q.remove() // 'There'
+
+const Queue = require("./queue");
+
+function weave(sourceOne, sourceTwo) {
+  let weavedQueue = new Queue();
+
+  while (sourceOne.peek() !== undefined || sourceTwo.peek() !== undefined) {
+    if (sourceOne.peek() !== undefined) {
+      weavedQueue.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek() !== undefined) {
+      weavedQueue.add(sourceTwo.remove());
+    }
+  }
+  return weavedQueue;
+}
+
+module.exports = weave;
